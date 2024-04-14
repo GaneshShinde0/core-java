752998064cc8a6a7bc3d0f1158ae4419734c80e5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-04-15 00:24:04 +0530

    Create IPAddressValidator.java

---
+++ Core Java Practice/src/com/ganesh/revision/IPAddressValidator.java
@@ -0,0 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class IPAddressValidator{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex{
+    public String pattern="^((25[0-5]|2[0-4]\\d|[01]?\\d{1,2})\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d{1,2})$";
+    
+    }
