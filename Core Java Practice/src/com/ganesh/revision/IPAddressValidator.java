f0de5c16833afdf4a11ecd6fb2e65358a7ab0bb5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-04-16 06:49:45 +0530

    Update IPAddressValidator.java

--- Core Java Practice/src/com/ganesh/revision/IPAddressValidator.java
+++ Core Java Practice/src/com/ganesh/revision/IPAddressValidator.java
@@ -18,4 +18,33 @@ class IPAddressValidator{
 class MyRegex{
     public String pattern="^((25[0-5]|2[0-4]\\d|[01]?\\d{1,2})\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d{1,2})$";
     
+    // Basic IPv4 Address Validation
+    public static final String BASIC_VALIDATION_REGEX = "^(\\d{1,3}\\.){3}\\d{1,3}$";
+
+    // Strict IPv4 Address Validation (0-255 for each octet)
+    public static final String STRICT_VALIDATION_REGEX = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+    // IPv4 Address Validation with Leading Zeros Allowed
+    public static final String LEADING_ZEROS_ALLOWED_REGEX = "^((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)$";
+
+    // IPv4 Address Validation without Leading Zeros
+    public static final String NO_LEADING_ZEROS_REGEX = "^((25[0-5]|2[0-4]\\d|[1-9]\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[1-9]\\d?)$";
+
+    // IPv4 Address Validation with Optional Leading Zeroes
+    public static final String OPTIONAL_LEADING_ZEROS_REGEX = "^((25[0-5]|2[0-4]\\d|1?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|1?\\d\\d?)$";
+
+    // IPv4 Address Validation with Dot Separators Escaped
+    public static final String ESCAPED_DOT_REGEX = "^(\\d{1,3}\\.){3}\\d{1,3}$";
+
+    // IPv4 Address Validation with Leading Zeros Ignored
+    public static final String LEADING_ZEROS_IGNORED_REGEX = "^((25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)$";
+
+    // IPv4 Address Validation with Single Digit Octets Allowed
+    public static final String SINGLE_DIGIT_OCTETS_REGEX = "^(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])$";
+
+    // IPv4 Address Validation with Optional Trailing Period
+    public static final String OPTIONAL_TRAILING_PERIOD_REGEX = "^(\\d{1,3}\\.){3}\\d{1,3}\\.?$";
+
+    // IPv4 Address Validation without Trailing Period
+    
     }
