ca8be31bacebe8970cfdea02dc238eca84a6dd68
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-04-20 16:33:22 +0530

    Create ExceptionHandlingBasics.java

---
+++ Core Java Practice/src/com/ganesh/revision/ExceptionHandlingBasics.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class ExceptionHandlingBasics {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        try{
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a/b);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }finally{
+            sc.close();
+        }
+    }
+}
